8584562f08260c600ace3283aa635469ac89160e
Tobias Gerlach <tobias.gerlacht@gmail.com>
2021-03-27 23:03:07 +0100

    Init

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("-v", "--verbose", help="increase output verbosity",
-                    action="store_true")
-args = parser.parse_args()
-if args.verbose:
-    print("verbosity turned on")
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--phase', type=str, default='train', help='train or test?')
+    parser.add_argument('--iteration', type=int, default=100000, help='The number of training iterations')
+    parser.add_argument('--dataset', type=str, default='eg', help='dataset_name')
+    return check_args(parser.parse_args())
+
+
+
+def test():
+    None
+def train():
+    None
+if __name__=="__main__":
+    print('LEts go!')
+    args = parse_args()
